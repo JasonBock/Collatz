1fc7f4314ce4c4b1f28ea0cf44a2649a5761146d
Jason Bock <jason.r.bock@outlook.com>
2021-11-27 17:12:21 -0600

    #1 added tests

---
+++ src/Collatz.Interactive.Tests/CollatzKernelExtensionTests.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.DotNet.Interactive;
+using Microsoft.DotNet.Interactive.CSharp;
+using Microsoft.DotNet.Interactive.Events;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Immutable;
+
+namespace Collatz.Interactive.Tests;
+
+// Lots of the code here was lifted from:
+// https://github.com/dotnet/interactive/blob/main/src/Microsoft.DotNet.Interactive.Tests/Utility/TestUtility.cs
+public static class CollatzKernelExtensionTests
+{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	private static KernelWrapper wrapper;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+	[SetUp]
+	public static async Task SetupAsync() =>
+#pragma warning disable CA2000 // Dispose objects before losing scope
+		CollatzKernelExtensionTests.wrapper = await KernelWrapper.CreateAsync().ConfigureAwait(false);
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+	[Test]
+	public static async Task SubmitCommandWithSOptionAsync()
+	{
+		using var events = CollatzKernelExtensionTests.wrapper.Kernel.KernelEvents.ToSubscribedList();
+		await CollatzKernelExtensionTests.wrapper.Kernel.SubmitCodeAsync("#!collatz -s 5").ConfigureAwait(false);
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(events.Count, Is.EqualTo(2));
+			Assert.That(events.Any(_ => _.GetType().Name == "CommandSucceeded"), Is.True);
+			Assert.That(events.Any(_ => _.GetType().Name == "DisplayedValueProduced"), Is.True);
+		});
+	}
+
+	[Test]
+	public static async Task SubmitCommandWithStartOptionAsync()
+	{
+		using var events = CollatzKernelExtensionTests.wrapper.Kernel.KernelEvents.ToSubscribedList();
+		await CollatzKernelExtensionTests.wrapper.Kernel.SubmitCodeAsync("#!collatz --start 5").ConfigureAwait(false);
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(events.Count, Is.EqualTo(2));
+			Assert.That(events.Any(_ => _.GetType().Name == "CommandSucceeded"), Is.True);
+			Assert.That(events.Any(_ => _.GetType().Name == "DisplayedValueProduced"), Is.True);
+		});
+	}
+
+	[Test]
+	public static async Task SubmitCommandWithUnknownOptionAsync()
+	{
+		using var events = CollatzKernelExtensionTests.wrapper.Kernel.KernelEvents.ToSubscribedList();
+		await CollatzKernelExtensionTests.wrapper.Kernel.SubmitCodeAsync("#!collatz --unknown 5").ConfigureAwait(false);
+
+		Assert.Multiple(() =>
+		{
+			Assert.That(events.Count, Is.EqualTo(3));
+			Assert.That(events.Any(_ => _.GetType().Name == "CodeSubmissionReceived"), Is.True);
+			Assert.That(events.Any(_ => _.GetType().Name == "CompleteCodeSubmissionReceived"), Is.True);
+			Assert.That(events.Any(_ => _.GetType().Name == "CommandSucceeded"), Is.True);
+		});
+	}
+
+	[TearDown]
+	public static void TearDown() =>
+		CollatzKernelExtensionTests.wrapper.Dispose();
+}
+
+public sealed class KernelWrapper
+	: IDisposable
+{
+	public static async Task<KernelWrapper> CreateAsync()
+	{
+		var wrapper = new KernelWrapper();
+		await wrapper.InitializeAsync().ConfigureAwait(false);
+		return wrapper;
+	}
+
+	public KernelWrapper()
+	{
+		this.Kernel = new CompositeKernel
+		{
+#pragma warning disable CA2000 // Dispose objects before losing scope
+			new CSharpKernel()
+#pragma warning restore CA2000 // Dispose objects before losing scope
+		};
+		this.KernelEvents = this.Kernel.KernelEvents.ToSubscribedList();
+	}
+
+	public async Task InitializeAsync() =>
+		await new CollatzKernelExtension().OnLoadAsync(this.Kernel).ConfigureAwait(false);
+
+	public void Dispose()
+	{
+		this.Kernel.Dispose();
+		this.KernelEvents.Dispose();
+	}
+
+	public Kernel Kernel { get; }
+	public SubscribedList<KernelEvent> KernelEvents { get; }
+}
+
+public sealed class SubscribedList<T>
+	: IReadOnlyList<T>, IDisposable
+{
+	private ImmutableArray<T> list = ImmutableArray<T>.Empty;
+	private readonly IDisposable subscription;
+
+	public SubscribedList(IObservable<T> source) =>
+		this.subscription = source.Subscribe(x => { this.list = this.list.Add(x); });
+
+	public void Dispose() => this.subscription.Dispose();
+
+	public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)this.list).GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+	public int Count => this.list.Length;
+
+	public T this[int index] => this.list[index];
+}
+
+public static class ObservableExtensions
+{
+	public static SubscribedList<T> ToSubscribedList<T>(this IObservable<T> self) =>
+		new(self);
+}
